8b888364a4fa30c1a9d66c55fd999a8fb40d4303
kaoutar <elazamikaoutar@gmail.com>
2024-11-26 13:25:30 +0100

    adding the four components adding listing footer header

---
+++ src/app/adding/adding.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-adding',
+  imports: [],
+  templateUrl: './adding.component.html',
+  styleUrl: './adding.component.css'
+})
+export class AddingComponent {
+
+}
