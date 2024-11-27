ec2a5c6c5d9df972b27947d3d2671955996626e7
kaoutar <elazamikaoutar@gmail.com>
2024-11-27 13:15:29 +0100

    employee service

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Employee } from '../models/Employee';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
+  private apiURL = "http://localhost:8080/api/employee"
 
-  constructor() { }
+
+  constructor(private http:HttpClient) { }
+
+  getEmpleados():Observable<Employee[]>{
+    return this.http.get<Employee[]>(this.apiURL);
+  }
+
+  
 }
