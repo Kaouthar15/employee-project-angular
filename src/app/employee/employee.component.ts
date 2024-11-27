37fff047d90c50d9f44caa00662a8d7cb2fd8bcc
kaoutar <elazamikaoutar@gmail.com>
2024-11-27 12:51:36 +0100

    co

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AddingComponent } from '../adding/adding.component';
 import { ListingComponent } from '../listing/listing.component';
+import { HeaderComponent } from "../header/header.component";
 @Component({
   selector: 'app-employee',
-  imports: [RouterOutlet, AddingComponent, ListingComponent],
+  imports: [RouterOutlet, AddingComponent, ListingComponent, HeaderComponent],
   templateUrl: './employee.component.html',
   styleUrl: './employee.component.css'
 })
